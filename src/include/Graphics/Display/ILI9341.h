45e97043eed054bf249544d1379684e90bd7505c
Slavey Karadzhov <slav@attachix.com>
2021-10-01 13:57:01 +0200

    Initial untested changes. Just drafting ideas.

--- src/include/Graphics/Display/ILI9341.h
+++ src/include/Graphics/Display/ILI9341.h
@@ -21,10 +21,7 @@
 
 #pragma once
 
-#include "../Device.h"
-#include <HSPI/Device.h>
-#include "../SpiDisplayList.h"
-#include <Digital.h>
+#include "Mipi.h"
 
 namespace Graphics
 {
@@ -32,30 +29,16 @@ namespace Display
 {
 class ILI9341Surface;
 
-class ILI9341 : private HSPI::Device, public Device, public RenderTarget
+class ILI9341 : public Mipi::Base
 {
 public:
 	static constexpr Size nativeSize{240, 320};
 
-	ILI9341(HSPI::Controller& spi) : HSPI::Device(spi)
-	{
-	}
+	using Mipi::Base::Base;
 
 	bool begin(HSPI::PinSet pinSet, uint8_t chipSelect, uint8_t dcPin, uint8_t resetPin = PIN_NONE,
-			   uint32_t clockSpeed = 4000000);
+			   uint32_t clockSpeed = 4000000) override;
 
-	using HSPI::Device::getSpeed;
-
-	uint32_t readRegister(uint8_t cmd, uint8_t byteCount);
-
-	uint32_t readDisplayId();
-	uint32_t readDisplayStatus();
-	uint8_t readPowerMode();
-	uint8_t readMADCTL();
-	uint8_t readPixelFormat();
-	uint8_t readImageFormat();
-	uint8_t readSignalMode();
-	uint8_t readSelfDiag();
 	uint16_t readNvMemStatus();
 
 	/* Device */
@@ -86,27 +69,8 @@ public:
 
 	Surface* createSurface(size_t bufferSize = 0) override;
 
-private:
+protected:
 	friend class ILI9341Surface;
-
-	using HSPI::Device::execute;
-
-	void execute(SpiDisplayList& list, DisplayList::Callback callback = nullptr, void* param = nullptr)
-	{
-		list.prepare(callback, param);
-		list.fillRequest();
-		execute(list.request);
-	}
-
-	static bool transferBeginEnd(HSPI::Request& request);
-
-	void reset(bool state)
-	{
-		if(resetPin == PIN_NONE) {
-			return;
-		}
-		digitalWrite(resetPin, !state);
-	}
 
 	HSPI::IoModes getSupportedIoModes() const override
 	{
@@ -114,13 +78,6 @@ private:
 	}
 
 	static bool staticRequestCallback(HSPI::Request& request);
-
-	uint8_t dcPin{PIN_NONE};
-	bool dcState{};
-	uint8_t resetPin{PIN_NONE};
-	uint8_t listIndex{0}; ///< Next display list to write to
-	void* param{nullptr};
-	AddressWindow addrWindow{};
 };
 
 } // namespace Display
